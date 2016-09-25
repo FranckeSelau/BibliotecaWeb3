e85b046da9ddd27d67f4dff5862d6207bd1add85
FranckeSelau <francke.selau@gmail.com>
2016-09-25 11:29:14 -0300

    relatorios

---
+++ src/java/bean/RelatorioMB.java
@@ -0,0 +1,71 @@
+package bean;
+
+import model.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named
+@ApplicationScoped //Application, pois os usuários cadastrados deverão permanecer mesmo se fizer logout.
+public class RelatorioMB {
+
+    //CRUD
+    private List<Usuario> listaUsuarios;
+    private Usuario usuarioSelecionado;
+
+    public RelatorioMB() {
+        usuarioSelecionado = new Usuario();
+        listaUsuarios = new ArrayList<Usuario>();
+        listaUsuarios.add(new Usuario("admin", "admin"));
+        listaUsuarios.add(new Usuario("francke", "admin"));
+        listaUsuarios.add(new Usuario("a", "a"));
+    }
+    
+    public Usuario getUsuarioSelecionado() {
+        return usuarioSelecionado;
+    }
+
+    public void setUsuarioSelecionado(Usuario usuarioSelecionado) {
+        this.usuarioSelecionado = usuarioSelecionado;
+    }
+
+    public List<Usuario> getListaUsuarios() {
+        return listaUsuarios;
+    }
+
+    public void setListaUsuarios(List<Usuario> listaUsuarios) {
+        this.listaUsuarios = listaUsuarios;
+    }
+    
+
+    public String novoUsuario(){
+        usuarioSelecionado=new Usuario();        
+        return("/admin/cadastroUsuarios?faces-redirect=true");
+    }
+
+    public String adicionarUsuario(){        
+        listaUsuarios.add(usuarioSelecionado);
+        this.novoUsuario();
+        return("/admin/confirmaCadastroUsuario?faces-redirect=true");
+    }
+    
+    public String mostrarRelatorios(){        
+        return("/admin/relatorios?faces-redirect=true");
+    }
+
+    public String editarUsuario(Usuario u){
+        usuarioSelecionado = u;
+        return("/admin/edicaoUsuarios?faces-redirect=true");
+    }
+    public String atualizarUsuario(){
+        return("/admin/listaUsuarios?faces-redirect=true");
+    }
+
+    public void removerUsuario(Usuario usuario){
+        listaUsuarios.remove(usuario);
+    }
+
+}
