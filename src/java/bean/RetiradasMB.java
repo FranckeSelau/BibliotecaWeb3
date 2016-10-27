9eda9a02fa2bfb3b97ec890186c21c39057a286e
FranckeSelau <francke.selau@gmail.com>
2016-10-27 15:28:36 -0200

    retiradas

---
+++ src/java/bean/RetiradasMB.java
@@ -0,0 +1,97 @@
+package bean;
+
+import java.io.Serializable;
+
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import model.Cliente;
+import model.Livro;
+import model.Retiradas;
+import rn.RetiradasRN;
+
+@Named
+@ApplicationScoped
+public class RetiradasMB implements Serializable {
+
+    @Inject
+    RetiradasRN retiradaRN;
+    private long id;
+    private String matricula, cliente;
+    private Cliente clienteSelecionado;
+    private Livro LivroSelecionado;
+
+    //CRUD
+    private List<Retiradas> listaRetiradas;
+    private Retiradas retiradaSelecionada;
+
+    public RetiradasMB() {
+        retiradaSelecionada = new Retiradas();
+     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public Retiradas getRetiradaSelecionada() {
+        return retiradaSelecionada;
+    }
+
+    public void setRetiradaSelecionada(Retiradas retiradaSelecionada) {
+        this.retiradaSelecionada = retiradaSelecionada;
+    }
+
+    public List<Retiradas> getListaRetiradas() {
+        return retiradaRN.listar();
+    }
+
+    public String novaRetirada() {
+        retiradaSelecionada = new Retiradas();
+        return ("/admin/retirada?faces-redirect=true");
+    }
+
+    public String novaRetiradaUsuario() {
+        retiradaSelecionada = new Retiradas();
+        return ("/usuario/retirada?faces-redirect=true");
+    }
+
+    public String adicionarRetirada() {
+        retiradaRN.salvar(retiradaSelecionada);
+        return (this.novaRetirada());
+    }
+
+    public String editarRetirada(Retiradas u) {
+        retiradaSelecionada = u;
+        return ("/admin/edicaoUsuarios?faces-redirect=true"); //fazer pagina
+    }
+
+    public String atualizarRetirada() {
+        retiradaRN.salvar(retiradaSelecionada);
+        return ("/admin/index?faces-redirect=true"); //fazer pagina
+    }
+
+    public void removerRetirada(Retiradas retirada) {
+        retiradaRN.remover(retirada);
+    }
+/*
+    public String verificaCliente() {
+        //Obtém o usuarioMB criado pelo servidor (nível de aplicação)
+        //UsuarioMB usuarioMB = (UsuarioMB) contexto.getExternalContext().getApplicationMap().get("usuarioMB");
+        //A partir do usuarioMB do servidor, pegamos a lista de usuários cadastrados no sistema
+        List<Cliente> listaClientes = clienteMB.getListaClientes();
+
+        for (Livro cliente : listaClientes) {
+            if (cliente.verificaCliente(matricula)) {
+                clienteSelecionado = cliente;
+            }
+        }
+
+        return ("admin/retirada?faces-redirect=true");
+    }*/
+
+}
