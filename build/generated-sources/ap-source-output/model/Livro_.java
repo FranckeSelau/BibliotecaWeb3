91bee9f1a73760b4b99f3682efeab479428fed4a
FranckeSelau <francke.selau@gmail.com>
2016-11-06 23:17:44 -0200

    ajustes css

---
+++ build/generated-sources/ap-source-output/model/Livro_.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-06T23:02:13")
+@StaticMetamodel(Livro.class)
+public class Livro_ { 
+
+    public static volatile SingularAttribute<Livro, String> ano;
+    public static volatile SingularAttribute<Livro, String> isbn;
+    public static volatile SingularAttribute<Livro, Date> dataLiberacao;
+    public static volatile SingularAttribute<Livro, Boolean> disponivel;
+    public static volatile SingularAttribute<Livro, String> nome;
+    public static volatile SingularAttribute<Livro, Long> id;
+    public static volatile SingularAttribute<Livro, String> editora;
+    public static volatile SingularAttribute<Livro, Integer> retiradas;
+    public static volatile SingularAttribute<Livro, String> autor;
+
+}
